affcca00c5913ff912b816cf8bc306af8b3fa010
ljorgens <leejorgensen22@gmail.com>
2015-03-06 13:24:18 -0800

    final commit

---
+++ controllers/emails.js
@@ -0,0 +1,16 @@
+// EmailClient.EmailsController = Ember.ArrayController.extend({
+// 	itemController: 'emails'
+// });
+
+// EmailClient.EmailsController = Ember.ObjectController.extend({
+// 	isClicked: false,
+// 	actions:{
+// 		showHide: function(){
+// 			if (this.isClicked){
+// 				this.set('isClicked', false);
+// 			} else {
+// 				this.set('isClicked', true);
+// 			}
+// 		}
+// 	}
+// });
